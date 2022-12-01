b202fd8163c63c5ff9134e0dc6da153ad77b7ba2
ZackarySantana <zackzackyack@gmail.com>
2022-12-01 12:31:35 -0500

    Default headers in layout

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,6 @@ export default function Head() {
     return (
         <>
             <title>This is title</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
         </>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
     return (
         <html lang="en">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
             <body>{children}</body>
         </html>
     );
